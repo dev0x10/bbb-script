d5e89577854dd0725aacfe8ce838df18e19da426
dev0x10 <dev.yauri@gmail.com>
2013-09-19 20:18:00 +0800

    remove script support for bbb 0.8 installation

--- bbb-setup.sh
+++ bbb-setup.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 
-source ~/.profile
-
-BBBHome="/home/$USER/dev/bigbluebutton"
-BBBDemoSite="/var/www/bigbluebutton-default"
-
-
 showMenu (){
 	clear
 	echo "BBB Setup Menu : "
-	echo "[1] Install BigBlueButton"
-	echo "[2] Setup Development Tools"
-	echo "[3] Checkout Source"
-	echo "[4] Setup Client Dev"
-	echo "[5] Setup API Dev"
-	echo "[6] Setup App Dev"
-	echo "[7] Build Client"
-	echo "[8] Show IP"
-	echo "[15] Install Matterhorn"
+	echo "[1] Install BigBlueButton 0.81"
+	echo "[2] Install Matterhorn"
 	echo "[x] Exit"
+}
+
+gototemp() {
+	if [ ! -d ~/tmp ]; then
+		mkdir ~/tmp
+	fi
+	cd ~/tmp
 }
 
 installbbb () {
@@ -26,156 +20,181 @@ installbbb () {
 	wget http://ubuntu.bigbluebutton.org/bigbluebutton.asc -O- | sudo apt-key add -
 
 	# Add the BigBlueButton repository URL and ensure the multiverse is enabled
-	echo "deb http://ubuntu.bigbluebutton.org/lucid_dev_08/ bigbluebutton-lucid main" | sudo tee /etc/apt/sources.list.d/bigbluebutton.list
-	echo "deb http://us.archive.ubuntu.com/ubuntu/ lucid multiverse" | sudo tee -a /etc/apt/sources.list
+	echo "deb http://ubuntu.bigbluebutton.org/lucid_dev_081/ bigbluebutton-lucid main" | sudo tee /etc/apt/sources.list.d/bigbluebutton.list
+	sudo apt-add-repository ppa:libreoffice/libreoffice-4-0
 
-	sudo apt-get update
-	sudo apt-get dist-upgrade
+	sudo apt-get -y update
+	sudo apt-get -y dist-upgrade
 
-	######## BIGBLUEBUTTON INSTALLATION
-	sudo apt-get -f -q -y install bigbluebutton
-	sudo apt-get -f -q -y install bbb-demo
+	installLibreOffice
+
+	installRuby
+
+	sudo apt-get -y install bigbluebutton
+	sudo apt-get -y install bbb-demo
+	sudo apt-get -y install bbb-playback-matterhorn
+	
+	//configure playback with matterhorn
+	ip=`ifconfig eth0 | sudo sed -n 's/.*dr:\(.*\)\s Bc.*/\1/p'`
+	sudo sed -i "s/192.168.0.147/$ip/g" /usr/local/bigbluebutton/core/scripts/matterhorn.yml
+	ssh-keygen -t rsa
+	sudo cp ~/.ssh/id_rsa /usr/local/bigbluebutton/core/scripts/matt_id_rsa
+	sudo chmod 600 /usr/local/bigbluebutton/core/scripts/matt_id_rsa
+	sudo chown tomcat6:tomcat6 /usr/local/bigbluebutton/core/scripts/matt_id_rsa
+	sudo mkdir /root/.ssh
+	sudo cp ~/.ssh/id_rsa.pub /root/.ssh/authorized_keys
+	
 	sudo bbb-conf --clean
 	sudo bbb-conf --check
-	echo "Please enter any key to continue"
+	
+	echo
+	echo "BigBlueButton installed. Press any key to continue..."
 	pause
 }
 
-installRuby(){
-	######## RUBY INSTALLATION
-	sudo apt-get install -f -q -y zlib1g-dev libssl-dev libreadline5-dev libyaml-dev build-essential bison checkinstall libffi5 gcc checkinstall libreadline5 libyaml-0-2
-	cd /tmp
-	wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.2-p290.tar.gz
-	tar xvzf ruby-1.9.2-p290.tar.gz
-	cd ruby-1.9.2-p290
-	./configure --prefix=/usr\
-	            --program-suffix=1.9.2\
-	            --with-ruby-version=1.9.2\
-	            --disable-install-doc
-	make
-	sudo checkinstall -D -y\
-	                  --fstrans=no\
-	                  --nodoc\
-	                  --pkgname='ruby1.9.2'\
-	                  --pkgversion='1.9.2-p290'\
-	                  --provides='ruby'\
-	                  --requires='libc6,libffi5,libgdbm3,libncurses5,libreadline5,openssl,libyaml-0-2,zlib1g'\
-	                  --maintainer=brendan.ribera@gmail.com
+installLibreOffice() {
+	gototemp
+	sudo apt-get -y remove --purge openoffice.org-*
+	wget http://bigbluebutton.googlecode.com/files/openoffice.org_1.0.4_all.deb
+	sudo dpkg -i openoffice.org_1.0.4_all.deb
+	sudo apt-get -y autoremove
+
+	sudo apt-get -y nstall python-software-properties
+	sudo apt-get -y install libreoffice-common
+	sudo apt-get -y install libreoffice
+}
+
+installYasm(){
+	cd /usr/local/src
+	sudo apt-get -y install build-essential git-core checkinstall yasm texi2html libopencore-amrnb-dev libopencore-amrwb-dev libsdl1.2-dev libtheora-dev libvorbis-dev libx11-dev libxfixes-dev libxvidcore-dev zlib1g-dev
+	sudo wget http://www.tortall.net/projects/yasm/releases/yasm-1.2.0.tar.gz
+	sudo tar xzvf yasm-1.2.0.tar.gz
+	cd yasm-1.2.0
+	sudo ./configure
+	sudo make
+	sudo checkinstall --pkgname=yasm --pkgversion="1.2.0" --backup=no --deldoc=yes --default
+}
+
+installLibvpx() {
+	# Setup libvpx
+	if [ ! -d /usr/local/src/libvpx ]; then
+	  cd /usr/local/src
+	  sudo git clone http://git.chromium.org/webm/libvpx.git
+	  cd libvpx
+	  sudo ./configure --enable-shared
+	  sudo make
+	  sudo make install
+	fi	
+}
+
+installX264() {
+	#Install X264
+	if [ ! -d /usr/local/src/x264 ]; then
+		cd /usr/local/src
+		sudo git clone git://git.videolan.org/x264
+		cd x264/
+		sudo ./configure --enable-shared
+		sudo make
+		sudo make install
+	fi
+}
+
+
+installffmpeg(){
+	
+	installYasm
+	installLibvpx
+	installX264
+
+	# Install ffmpeg
+	cd /usr/local/src
+	sudo wget http://ffmpeg.org/releases/ffmpeg-0.11.3.tar.gz
+	sudo tar -xvzf ffmpeg-0.11.3.tar.gz
+	cd ffmpeg-0.11.3
+	sudo ./configure --enable-gpl --enable-version3 --enable-nonfree --enable-postproc --enable-libfaac --enable-libopencore-amrnb --enable-libopencore-amrwb --enable-libtheora --enable-libvorbis --enable-libx264 --enable-libxvid --enable-x11grab --enable-libmp3lame --enable-libvpx --enable-shared
+	sudo make
+	sudo make install
+	sudo checkinstall --pkgname=ffmpeg --pkgversion="5:$(./version.sh)" --backup=no --deldoc=yes --default
+}
+
+installRuby() {
+	gototemp
+	wget https://bigbluebutton.googlecode.com/files/ruby1.9.2_1.9.2-p290-1_amd64.deb
+
+	sudo apt-get install libreadline5 libyaml-0-2
+	sudo dpkg -i ruby1.9.2_1.9.2-p290-1_amd64.deb
+
 	sudo update-alternatives --install /usr/bin/ruby ruby /usr/bin/ruby1.9.2 500 \
 	                         --slave /usr/bin/ri ri /usr/bin/ri1.9.2 \
 	                         --slave /usr/bin/irb irb /usr/bin/irb1.9.2 \
 	                         --slave /usr/bin/erb erb /usr/bin/erb1.9.2 \
 	                         --slave /usr/bin/rdoc rdoc /usr/bin/rdoc1.9.2
+	                         
 	sudo update-alternatives --install /usr/bin/gem gem /usr/bin/gem1.9.2 500
-	sudo rm /tmp/ruby-1.9.2-p290.tar.gz
-	######## END OF RUBY INSTALLATION
 }
 
-checkoutsource(){
-	git clone https://github.com/bigbluebutton/bigbluebutton.git
-	cd bigbluebutton
-	git checkout -b develop v0.8
-	echo "Press any key to continue"
-	pause	
-}
+installMatterhorn(){
 
-setuptools (){
-	cd ~
-	if [ ! -d dev/ ]; then
-		mkdir dev
-	fi
-	cd dev
-	bbb-conf --setup-dev tools
-	source ~/.profile
-	echo "Press any key to continue"
-	pause	
-}
-
-setupclientdev() {
-	bbb-conf --setup-dev client
-	echo "Press any key to continue"
-	pause
-}
-
-setupapidev() {
-	bbb-conf --setup-dev web
-	cd $BBBHome/bigbluebutton-web
-	stopapi
-	grails -Dserver.port=8888 run-app
-	stopapi
-	startapi
-	echo "Press any key to continue"
-	pause
-}
-
-startapi() {
-    nohup grails -Dserver.port=8888 run-app &
-}
-
-stopapi() {
-	service=$(sudo netstat -nap|grep 8888 | awk '{ print $7}' | cut -d/ -f1)
-    if [ -n "$service" ]; then
-    	kill $service
-    fi
-}
-
-setupappdev() {
-	sudo chmod -R 777 /usr/share/red5/webapps
-	sudo service red5 stop
-	bbb-conf --setup-dev apps
-	mv "$BBBHome"/bigbluebutton-apps/build.gradle "$BBBHome"/bigbluebutton-apps/build.gradle.BAK
-	cp build.gradle.app "$BBBHome"/bigbluebutton-apps/build.gradle
-	gradle resolveDeps
-	gradle clean war deploy
-}
-
-buildclient() {
-	cd "$BBBHome"/bigbluebutton-client
-	if [ ! -d bin/ ]; then
-		mkdir bin
-	fi
-	ant locales
-	ant clean-build-all
-	echo "Press any key to continue"
-	pause
-}
-
-installmatterhorn(){
 	sudo mkdir -p /opt/matterhorn
 	sudo chown $USER:$GROUPS /opt/matterhorn
-	sudo apt-get -q -f -y install subversion
-	svn checkout https://opencast.jira.com/svn/MH/tags/1.3.1 /opt/matterhorn/matterhorn_1.3.1
-	sudo apt-get -q -f -y install maven2
-	mkdir tmp
-	cd tmp
-	wget http://archive.apache.org/dist/felix/org.apache.felix.main.distribution-3.2.2.tar.gz
-	tar xvf org.apache.felix.main.distribution-3.2.2.tar.gz
-	mv felix-framework-3.2.2 /opt/matterhorn/felix
-	rm -R tmp
-	rm org.apache.felix.main.distribution-3.2.2.tar.gz
-	mkdir /opt/matterhorn/felix/load
-	cp -rf /opt/matterhorn/matterhorn_1.3.1/dlsocs/felix/* /opt/matterhorn/felix/
-	export MAVEN_OPTS='-Xms256m -Xmx960m -XX:PermSize=64m -XX:MaxPermSize=256m'
-	cd /opt/matterhorn/matterhorn_1.3.1/
-	mvn clean install -DdeployTo=/opt/matterhorn/felix/matterhorn
+	sudo apt-get -y install subversion
+	svn checkout https://opencast.jira.com/svn/MH/tags/1.4.0 /opt/matterhorn/1.4.0
+	ln -s /opt/matterhorn/1.4.0 /opt/matterhorn/felix
 	
-	echo
-	echo "Change server url "http://localhost" to your hostname"
-	ip=`ifconfig eth0 | sed -n 's/.*dr:\(.*\)\s Bc.*/\1/p'`
-	sed -i "s/org\.opencastproject\.server\.url=.*/org\.opencastproject\.server\.url=http:\/\/$ip:8080/g" /opt/matterhorn/felix/conf/config.properties
-	echo
-	echo "Change default port to 8181"
-	sed -i "s/8080/8181/g" /opt/matterhorn/felix/conf/config.properties
+	sudo apt-get install python-software-properties
+	sudo add-apt-repository ppa:webupd8team/java
+	sudo apt-get -y update
+	sudo apt-get -y install oracle-java7-installer
+	sudo apt-get -y install maven2
+
+	configureMatterhorn
+
+	export MAVEN_OPTS='-Xms256m -Xmx960m -XX:PermSize=64m -XX:MaxPermSize=256m'
+	cd /opt/matterhorn/1.4.0
+	mvn clean install -DdeployTo=/opt/matterhorn/1.4.0
 
 	echo "export M2_REPO=/home/$USER/.m2/repository" >> ~/.bashrc
 	echo "export FELIX_HOME=/opt/matterhorn/1.4.0" >> ~/.bashrc
 	echo "export JAVA_OPTS='-Xms1024m -Xmx1024m -XX:MaxPermSize=256m'" >> ~/.bashrc
-
+	
+	installMatterhorn3P
+	installffmpeg
+	
+	createMatterhornService
+	
 	echo "Load profile with : source ~/.bashrc before you run Matterhorn"
 	echo
 	echo "To Run Matterhorn : sh /opt/matterhorn/felix/bin/start_matterhorn.sh"
 	echo
 	echo "To Stop Matterhorn : sh /opt/matterhorn/felix/bin/shutdown_matterhorn.sh"
+	echo
+	echo "To run Matterhorn as service : sudo service matterhorn start"
+	pause
+}
+
+installMatterhorn3P(){
+	cd /opt/matterhorn/1.4.0/docs/scripts/3rd_party
+	./check-prereq
+	./download-sources
+	./linux-compile
+}
+
+configureMatterhorn() {
+	echo "Change server url "http://localhost" to your hostname"
+	ip=`ifconfig eth0 | sed -n 's/.*dr:\(.*\)\s Bc.*/\1/p'`
+	sed -i "s/org\.opencastproject\.server\.url=.*/org\.opencastproject\.server\.url=http:\/\/$ip:8080/g" /opt/matterhorn/1.4.0/etc/config.properties
+	echo
+	echo "Change default port to 8181"
+	sed -i "s/8080/8181/g" /opt/matterhorn/1.4.0/etc/config.properties
+	
+	sed -i "s/PICT_TYPE_I/I/g" /opt/matterhorn/felix/etc/encoding/engage-images.properties
+	sed -i "s/PICT_TYPE_I/I/g" /opt/matterhorn/felix/etc/encoding/feed-images.properties
+}
+
+createMatterhornService() {
+	sudo cp /opt/matterhorn/1.4.0/docs/scripts/init/matterhorn_init_d.sh /etc/init.d/matterhorn
+	sudo update-rc.d matterhorn defaults 99
+	sudo sed -i "s/\$\USER/root/g" /etc/init.d/matterhorn
 }
 
 showIP (){
@@ -197,13 +216,6 @@ do
 	echo
 	case $OPT in
 		1) installbbb ;;
-		2) setuptools ;;
-		3) checkoutsource ;;		
-		4) setupclientdev ;;
-		5) setupapidev ;;
-		6) setupappdev ;;
-		7) buildclient ;;
-		15) installmatterhorn;;
-		8) showIP ;echo "Press any key to continue";pause;
+		2) installMatterhorn ;;
 	esac
 done
